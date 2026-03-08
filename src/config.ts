29093130c2c15720f793fb7f53d4cef56b2fb3d7
BerOSilk <1990.khalilbaraa@gmail.com>
2026-03-08 16:10:43 +0200

    add server configuration type

---
+++ src/config.ts
@@ -0,0 +1,18 @@
+process.loadEnvFile();
+
+const config = {
+    PORT: getFromEnv("PORT") || 8080,
+}
+
+
+function getFromEnv(key: string) {
+    const value = process.env?.[key];
+    if (!value) {
+        console.log(`${key} is not initialized using default`);
+        return null;
+    } else {
+        return value;
+    }
+}
+
+export { config };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { config } from "./config.js";
 
 const app = express();
-const PORT = 8080;
 
-app.listen(PORT, () => console.log("Server running on:", PORT));
+app.use(express.json());
+
+app.get("/api/ping", (req: Request, res: Response) => {
+    res.status(200).send({
+        message: "Pong",
+    })
+});
+
+app.listen(config.PORT, () => console.log("Server running on:", config.PORT));
