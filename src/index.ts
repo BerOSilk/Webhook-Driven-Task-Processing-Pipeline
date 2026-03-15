6a817298404c434e8e9b8b07f8fda0a7587c1f59
BerOSilk <1990.khalilbaraa@gmail.com>
2026-03-15 13:01:28 +0200

    add Job APIs & Docker compose files

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import {
 } from "./controllers/pipelines.js";
 import { initializeWorker, stopWorker } from "./worker/index.js";
 import { handleWebhook } from "./controllers/webhook.js";
+import { GetJob, GetJobDeliveries, GetJobs } from "./controllers/jobs.js";
 
 const app = express();
 
@@ -28,16 +29,23 @@ app.delete("/api/pipeline/:id", DeletePipelines);
 app.post("/api/webhook/:id", handleWebhook);
 app.get("/api/webhook/:id", handleWebhook);
 
+app.get("/api/job", GetJobs);
+app.get("/api/job/:id", GetJob);
+app.get("/api/job/:id/deliveries", GetJobDeliveries);
+
 app.use(errorHandler);
 
 initializeWorker(config.INTERVAL_MS as number, config.MAX_ATTEMPTS as number);
 
 app.listen(config.PORT, () => console.log("Server running on:", config.PORT));
 
-process.on("SIGINT", () => {
+function shutdown() {
   console.log("Shutting down server");
 
   stopWorker();
   console.log("Server Stopped");
   process.exit(1);
-});
+}
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
